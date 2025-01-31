e95faaebc4d9e3dbfb3fc7c9c1e50092f4db7145
rschmutz.pomy <rschmutz.pomy@gmail.com>
2025-01-31 10:01:34 +0000

    Create Branch Tristan + Add tests code in lobby page

--- client/src/modules/games/list.jsx
+++ client/src/modules/games/list.jsx
@@ -40,8 +40,41 @@ const GamesList = () => {
 
         </ul>
         
-        <hr />        
+        <hr />
+        <br />         
     
+        <ul>
+            <li><Link className="text-blue-500 underline" to="/lobby/join-game">Lien de connexion sur une partie existante (bouton) </Link></li>
+            <li><Link className="text-blue-500 underline" to={`/games/${MOCKED_GAME_ID}`}>Lien de connexion sur une nouvelle partie (bouton) </Link></li>
+        </ul>
+
+        <hr />
+
+        <ul>
+            <li>Liste des parties en cours :</li>
+            <li>Partie #1<Link className="text-blue-500 underline" to={`/games/${MOCKED_GAME_ID}`}>Continuer la partie</Link></li>
+            <li>Partie #2<Link className="text-blue-500 underline" to={`/games/${MOCKED_GAME_ID}`}>Continuer la partie</Link></li>
+            <li>Partie #3<Link className="text-blue-500 underline" to={`/games/${MOCKED_GAME_ID}`}>Continuer la partie</Link></li>
+            <li>Partie #4<Link className="text-blue-500 underline" to={`/games/${MOCKED_GAME_ID}`}>Continuer la partie</Link></li>
+            <li>Partie #5<Link className="text-blue-500 underline" to={`/games/${MOCKED_GAME_ID}`}>Continuer la partie</Link></li>
+            <li>Partie #42<Link className="text-blue-500 underline" to={`/games/${MOCKED_GAME_ID}`}>Continuer la partie</Link></li>
+        </ul>
+
+        <hr />
+
+        <ul>
+            <li>Liste des parties terminées :</li>
+            <li>Partie #1<Link className="text-blue-500 underline" to={`/games/${MOCKED_GAME_ID}/ended`}>Résultat de la partie</Link></li>
+            <li>Partie #2 <Link className="text-blue-500 underline" to={`/games/${MOCKED_GAME_ID}/ended`}>Résultat de la partie</Link></li>
+            <li>Partie #3 <Link className="text-blue-500 underline" to={`/games/${MOCKED_GAME_ID}/ended`}>Résultat lol de la partie</Link></li>
+            <li>Partie #4 <Link className="text-blue-500 underline" to={`/games/${MOCKED_GAME_ID}/ended`}>Résultat de la partie</Link></li>
+            <li>Partie #5 <Link className="text-blue-500 underline" to={`/games/${MOCKED_GAME_ID}/ended`}>Résultat de la partie</Link></li>
+            <li>Partie #42 <Link className="text-blue-500 underline" to={`/games/${MOCKED_GAME_ID}/ended`}>Résultat de la partie</Link></li>
+        </ul>
+
+        <hr />        
+        <p><Link className="text-blue-500 underline" to={`/login`}>Retour au menu (déconnexion)</Link></p>
+        <br />
     </>)
 
 }
