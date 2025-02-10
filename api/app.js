3a8596a979bedad0f8ab9797817dee47ddf1445e
Ken Cacciabue <ken.cacciabue@formation.orif.ch>
2025-02-10 16:00:20 +0100

    ajout de l'objet Game qui represente la party

--- api/app.js
+++ api/app.js
@@ -12,7 +12,7 @@ const userRoutes = require('./routes/user')
 
 // connection à la base de donnée mongodb, plus précisément le cluster 0 avec l'utilisateur KenCacciabue
 mongoose.set('strictQuery', false);
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/?retryWrites=true&w=majority&appName=${process.env.DB_APP_NAME}`)
+mongoose.connect(`mongodb+srv://KenBattle:OhMgJYjh8g04Y27K@clusterbattle.zjpdt.mongodb.net/?retryWrites=true&w=majority&appName=ClusterBattle`)
     // S i tout se passe sans probleme, affiche le succès de la connection dans la console
     .then(() => console.log('Connexion à MongoDB réussie !'))
     // si une erreure est reperer, affiche l'erreur
---
+++ api/models/Game.js
@@ -0,0 +1,15 @@
+// import mongoose
+const mongoose = require('mongoose')
+
+// crée un schema definissant une Party
+const gameSchema = mongoose.Schema({
+    // text à propos de l'état de la partie
+    state: { type: String, required: true},
+    // mail de l'utilisateur qui à créé la party
+    host: { type: String, required: true},
+    // nom d'ut'ilisateur du challenger
+    challenger: { type: String, required: false} 
+})
+
+// exporte le shema de la party en tant que Game
+module.exports = mongoose.model('Game', gameSchema)
