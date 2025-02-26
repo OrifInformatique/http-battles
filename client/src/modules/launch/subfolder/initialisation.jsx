d740d05707d1f443960fd05931eff7fa30bef0ff
YaelPomy2 <yaelfavresectioninformatique@gmail.com>
2025-02-26 14:16:31 +0100

    Essais pour ul / li - liste déroulantes

--- client/src/modules/launch/subfolder/initialisation.jsx
+++ client/src/modules/launch/subfolder/initialisation.jsx
@@ -4,38 +4,41 @@ import '../../general.css';
 
 function MyList({ handleChange, valeurs = [], i, tech}) {
     return (
-        <select name={`d${i}.${tech}`} onChange={handleChange} style={{ color: "black" }}>
-            {valeurs.map((valeur, index) => (
-                <option key={index}>{valeur}</option>
-            ))}
-        </select>
+        <ul style={{ color: "black" }}>
+             {valeurs.map((valeur, index) => (
+        <li key={index} onClick={() => handleChange({ target: { name: `d${i}.${tech}`, value: valeur } })} style={{ cursor: "pointer" }}>
+            {valeur}
+        </li>
+    ))}
+</ul>
     );
 }
 
 function InitialisationList({ handleChange, staticVal1, staticVal2, dynamicVal, i, phrase}) {
+console.log(dynamicVal[1])
+    return (
+    <>
+        <ul className="" defaultValue={phrase.word} onChange={handleChange} name={`d${i}.word`} style={{ color: "black"}}>
+            {dynamicVal[i-1] && dynamicVal[i-1].map((value, index) => (
+                <il key={index} value={value}>
+                    {value}
+                </il>
+            ))}
+        </ul>
 
-return (
-<>
-    <select className="" defaultValue={phrase.word} onChange={handleChange} name={`d${i}.word`} style={{ color: "black"}}>
-        {dynamicVal[i-1] && dynamicVal[i-1].map((value, index) => (
-            <option key={index} value={value}>
-                {value}
-            </option>
-        ))}
-    </select>
-    <div style={{display:"flex",flexFlow:"row", gap:"10px"}}>
-        <div style={{display:"flex",flexFlow:"column"}}>
-            <h3>Route</h3>
-            <MyList handleChange={handleChange} tech={"route"} valeurs={staticVal1} i={i}/> 
-        </div>
+        <div style={{display:"flex",flexFlow:"row", gap:"10px"}}>
+            <div style={{display:"flex",flexFlow:"column"}}>
+                <h3>Route</h3>
+                <MyList handleChange={handleChange} tech={"route"} valeurs={staticVal1} i={i}/> 
+            </div>
 
-        <div style={{display:"flex",flexFlow:"column"}}>
-        <h3>Methode</h3>
-        <MyList handleChange={handleChange} tech={"method"} valeurs={staticVal2} i={i}/>
-        <br/><br/><br/><br/>
+            <div style={{display:"flex",flexFlow:"column"}}>
+            <h3>Methode</h3>
+            <MyList handleChange={handleChange} tech={"method"} valeurs={staticVal2} i={i}/>
+            <br/><br/><br/><br/>
+            </div>
         </div>
-    </div>
-</>);
+    </>);
 }
 
 function PhraseLaunch({ handleChange ,staticVal1, staticVal2, dynamicVal, i, phrase}) {
