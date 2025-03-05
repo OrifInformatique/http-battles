43a0c1c23c654721ffbacc6af4e99c5dd12d0826
YaelPomy2 <yaelfavresectioninformatique@gmail.com>
2025-03-05 16:46:50 +0100

    Toujours sur mes listes déroulantes
    J'ai pu commencer à mettre en place les indications que vous m'aviez fournies. J'ai pu également un peu optimiser le code et mes div.

--- client/src/modules/launch/subfolder/initialisation.jsx
+++ client/src/modules/launch/subfolder/initialisation.jsx
@@ -8,7 +8,7 @@ function MyList({ handleChange, valeurs = [], i, tech, dynamicVal}) {
         case "dynamicVal":
             console.log("dynamicVal")
             return(<>
-                <ul className="initUl" style={{ color: "black", backgroundColor: "white"}}>
+                <ul className="initUl" style={{ color: "black"}}>
                     {valeurs[i-1] && valeurs[i-1].map((value, index) => (
                     <li className="initLi" key={index} onClick={() => handleChange({ target: { name: `${i}.${tech}` } })}>
                         {value}
@@ -33,32 +33,44 @@ function MyList({ handleChange, valeurs = [], i, tech, dynamicVal}) {
 
 function InitialisationList({ handleChange, staticVal1, staticVal2, dynamicVal, i, phrase, tech}) {
     
-    return (<>
-        <ul className="initUl" style={{ color: "black", backgroundColor: "transparent"}}>
-            {dynamicVal[i-1].map((value, index) => (
-                <li className="initLi" key={index} onClick={() => handleChange({ target: { name: `${i}.${tech}` } })}>
-                    {value}
-                </li>
-            ))}
-        </ul>
-
-    <div className="initPosition">
-        <div className="initWord"style={{display:"flex",flexFlow:"column"}}>
+    {/*
+    <div className="initContainerVisible">
+        <div className="Container">
+            <div className="initWord"style={{display:"flex",flexFlow:"column"}}>
             <h3>Mot {i}</h3>
             <MyList handleChange={handleChange} tech={"dynamicVal"} valeurs={dynamicVal} i={i}/> 
-        </div>
+            </div>
 
-        <div className="initRoute"style={{display:"flex",flexFlow:"column"}}>
+            <div className="initRoute" style={{display:"flex",flexFlow:"column"}}>
             <h3>Route</h3>
             <MyList handleChange={handleChange} tech={"route"} valeurs={staticVal1} i={i}/> 
-        </div>
+            </div>
 
-        <div className="initMethod" style={{display:"flex",flexFlow:"column"}}>
-        <h3>Methode</h3>
-        <MyList handleChange={handleChange} tech={"method"} valeurs={staticVal2} i={i}/>
-        <br/><br/><br/><br/>
+            <div className="initMethod" style={{display:"flex",flexFlow:"column"}}>
+            <h3>Methode</h3>
+            <MyList handleChange={handleChange} tech={"method"} valeurs={staticVal2} i={i}/>
+            <br/><br/><br/><br/>
+            </div>
         </div>
-    </div>
+    </div>*/}
+
+    return (<>
+        <div className="initContainerVisible">
+                <div className="initWord"style={{display:"flex",flexFlow:"column"}}>
+                    <h3>Mot {i}</h3>
+                    <MyList handleChange={handleChange} tech={"dynamicVal"} valeurs={dynamicVal} i={i}/> 
+                </div>
+                
+                <div className="initRoute" style={{display:"flex",flexFlow:"column"}}>
+                    <h3>Route</h3>
+                    <MyList handleChange={handleChange} tech={"route"} valeurs={staticVal1} i={i}/> 
+                </div>
+
+                <div className="initMethod" style={{display:"flex",flexFlow:"column"}}>
+                    <h3>Methode</h3>
+                    <MyList handleChange={handleChange} tech={"method"} valeurs={staticVal2} i={i}/>
+                </div>
+        </div>
     </>);
 }
 
@@ -81,22 +93,13 @@ export default ({handleChange, dynamicVal, staticVal1, staticVal2, phrase, setIs
 
             <form id="myForm" onSubmit={(e) => {e.preventDefault(), setIsSubmitted(2)}}>
 
-                    {/*Liste*/}
-                    <div className="initListLaunchContainer" >
-                        <div className="initFirstListLaunchContainer"> 
-                            
-                            <div className="initInitialisationList1"> <InitialisationList handleChange={handleChange} dynamicVal={dynamicVal} staticVal1={staticVal1} staticVal2={staticVal2} i={1} phrase={phrase[1]}/> </div>
-                            <div className="initInitialisationList2"> <InitialisationList handleChange={handleChange} dynamicVal={dynamicVal} staticVal1={staticVal1} staticVal2={staticVal2} i={2} phrase={phrase[2]}/> </div>
-                            <div className="initInitialisationList3"> <InitialisationList handleChange={handleChange} dynamicVal={dynamicVal} staticVal1={staticVal1} staticVal2={staticVal2} i={3} phrase={phrase[3]}/> </div> 
-                        </div>
-                        <div className="initThirdListLaunchContainer">
-
-                        <div className="initInitialisationList4"> <InitialisationList handleChange={handleChange} dynamicVal={dynamicVal} staticVal1={staticVal1} staticVal2={staticVal2} i={4} phrase={phrase[4]}/> </div>
-                        <div className="initInitialisationList5"> <InitialisationList handleChange={handleChange} dynamicVal={dynamicVal} staticVal1={staticVal1} staticVal2={staticVal2} i={5} phrase={phrase[5]}/> </div>
-                        
-                        </div>
-                    </div>
-
+                <div className="initContentContainer">                     
+                          <InitialisationList handleChange={handleChange} dynamicVal={dynamicVal} staticVal1={staticVal1} staticVal2={staticVal2} i={1} phrase={phrase[1]}/> 
+                          <InitialisationList handleChange={handleChange} dynamicVal={dynamicVal} staticVal1={staticVal1} staticVal2={staticVal2} i={2} phrase={phrase[2]}/> 
+                          <InitialisationList handleChange={handleChange} dynamicVal={dynamicVal} staticVal1={staticVal1} staticVal2={staticVal2} i={3} phrase={phrase[3]}/> 
+                        <InitialisationList handleChange={handleChange} dynamicVal={dynamicVal} staticVal1={staticVal1} staticVal2={staticVal2} i={4} phrase={phrase[4]}/> 
+                        <InitialisationList handleChange={handleChange} dynamicVal={dynamicVal} staticVal1={staticVal1} staticVal2={staticVal2} i={5} phrase={phrase[5]}/>
+                </div> 
             </form>
             {/* <div className="initFirstListLaunchContainer"> 
                     <InitialisationList handleChange={handleChange} dynamicVal={dynamicVal} staticVal1={staticVal1} staticVal2={staticVal2} i={1} phrase={phrase[1]}/>
