6a224a579fe4c65672f6692ebe33f494dba44a96
Ken Cacciabue <ken.cacciabue@formation.orif.ch>
2025-03-17 13:26:44 +0100

    commentaires util/log et amélioration methode listLogs

--- api/util/log.js
+++ api/util/log.js
@@ -15,6 +15,8 @@ const utilUser = require('../util/user')
 // location global pour la gestion d'erreur
 const LOC_GLOB = "file: ../util/log"
 
+
+// crée un objet log
 exports.logObjectCreation = async (log) => {
 
     // cée un objet de log
@@ -29,22 +31,29 @@ exports.logObjectCreation = async (log) => {
         data: log.data
     })
 
+    // retourne l'objet log
     return logObject
 }
 
+// enregistre un objet log dans la base de donnée
 exports.logObjectSave = async (log) => {
-
+    // enregistre le log dans la base données et retourn l'objet enregistré
     const savedLog = await log.save()
 
+    // retourn l'objet enregistré
     return savedLog
 }
 
+// crée un objet log et l'enregistre dans la base donnée
 exports.logToDatabase = async (log) => {
 
+    // crée un objet log
     const createdLog = await this.logObjectCreation(log)
 
+    // enregistre un objet log dans la base de donnée
     const savedLog = await this.logObjectSave(createdLog)
 
+    // retourn l'objet enregistré
     return savedLog
 }
 
@@ -52,15 +61,17 @@ exports.logInitFindUserAndGame = async (req) => {
     // renseigne dans quel méthode les futur erreures sont
     const LOC_LOC = "methode: logInitFindUserAndGame"
 
-    if(req.package === undefined){
+    // vérifie si la requete à déjà un package et en crée un si non
+    if (req.package === undefined) {
         req.package = {}
     }
 
+    // vérifie si la requete à un utilisateur
     if (req.auth.userId !== undefined) {
         // récupère les donnée utilisateur du client
         await utilUser.getUserById(req.auth.userId, req)
             .then(value => {
-                req.user = value
+                // stoque le client dans la requete
                 req.package.user = value
 
                 req.data.push({
@@ -79,11 +90,12 @@ exports.logInitFindUserAndGame = async (req) => {
             })
     }
 
+    // vérifie si la requete contient l'id d'une partie
     if (req.body.gameId !== undefined) {
         // récupère les données de la partie
         await middleGame.getGame(req)
             .then(value => {
-                req.game = value
+                // stoque la partie dans la requete
                 req.package.game = value
 
                 req.data.push({
@@ -102,6 +114,7 @@ exports.logInitFindUserAndGame = async (req) => {
             })
     }
 
+    // retourn le package
     return req.package
 }
 
@@ -109,7 +122,8 @@ exports.logDate = async (req) => {
     // renseigne dans quel méthode les futur erreures sont
     const LOC_LOC = "methode: logDate"
 
-    if(req.package === undefined){
+    // vérifie si la requete à déjà un package et en crée un si non
+    if (req.package === undefined) {
         req.package = {}
     }
 
@@ -121,182 +135,251 @@ exports.logDate = async (req) => {
     req.package.hour = req.package.date.getHours() + 1
     req.package.minute = req.package.date.getMinutes() + 1
 
+    // retourn le package
     return req.package
 }
 
+// construit le log
 exports.logConstructor = async (req) => {
     // renseigne dans quel méthode les futur erreures sont
     const LOC_LOC = "methode: logConstructor"
 
-
+    // crée le log
     req.log = {}
 
+    // test si la requete a un body, si oui, l'ajoute au log
     if (req.body !== undefined) {
         req.log.body = req.body
     }
 
+    // test si la requete a un utitlisateur, si oui, l'ajoute au log
     if (req.package.user !== undefined) {
         req.log.user = req.package.user
     }
 
+    // test si la requete a une partie, si oui, l'ajoute au log
     if (req.package.game !== undefined) {
         req.log.game = req.package.game
     }
 
+    // test si la requete a une date, si oui, l'ajoute à la requete
     if (req.package.date !== undefined) {
         req.log.date = req.package.date
     }
 
+    // test si la requete a une année, si oui, l'ajoute à la requete
     if (req.package.year !== undefined) {
         req.log.year = req.package.year
     }
 
+    // test si la requete a un mois, si oui, l'ajoute à la requete
     if (req.package.month !== undefined) {
         req.log.month = req.package.month
     }
 
+    // test si la requete a un jour, si oui, l'ajoute à la requete
     if (req.package.day !== undefined) {
         req.log.day = req.package.day
     }
 
+    // test si la requete a une heure, si oui, l'ajoute à la requete
     if (req.package.hour !== undefined) {
         req.log.hour = req.package.hour
     }
 
+    // test si la requete a une minute, si oui, l'ajoute à la requete
     if (req.package.hour !== undefined) {
         req.log.minute = req.package.minute
     }
 
+    // test si la requete a des données, si oui, les ajoute à la requete
     if (req.data !== undefined) {
         req.log.data = req.data.slice()
     }
 }
 
+// list les logs dans la base donées
 exports.listLogs = async (req) => {
+    // crée un objet user
     var user = {}
+    // crée un objet game
     var game = {}
+    // crée un objet query qui servira à faire une requette pour la base de données
     var query = {}
 
+    // test si l'id d'un log spécifique est demandé par le biais de son id
     if (req.body.logId !== undefined) {
+        // si oui, l'ajoute au query en traduisan l'id en object id qui sera reconu par la base de donnée et en l'ajoutant au query
         var logId = {
             "_id": mongoose.Types.ObjectId(req.body.logId)
         }
         Object.assign(query, logId)
     }
 
+    // test si les log d'un utilisateur particulier son demander par le biais de son id
     if (req.auth.userId !== undefined) {
+        // si oui, l'ajoute au query en traduisan l'id en object id qui sera reconu par la base de donnée et en l'ajoutant au query
         user.userId = {
             "user._id": mongoose.Types.ObjectId(req.auth.userId)
         }
         Object.assign(query, user.userId)
     }
 
+    // test si les log d'un utilisateur ayant un email particulier son demandé
     if (req.body.email !== undefined) {
+        // stoque la l'email dans un objet
         user.email = {
             "user.email": req.body.email
         }
+        // ajoute le contenu de l'objet au query
         Object.assign(query, user.email)
     }
 
+    // test si le client demand les log d'un utilisateur avec un mot de pass particulier
     if (req.body.password !== undefined) {
+        // stoque la requet pour la base de donnée dans un objet
         user.password = {
             "user.password": req.body.password
         }
+        // asigne le contenu de l'objet au query
         Object.assign(query, user.password)
     }
 
+    // test si le client recherche les log d'un utilisateur avec un username particulier
     if (req.body.username !== undefined) {
+        // stoque la requete pour la base de donnée dans un objet
         user.username = {
             "user.username": req.body.username
         }
+        // stoque le contenu de l'objet dans le query
         Object.assign(query, user.username)
     }
 
+    // test si le client demande les log d'un utilisateur avec un prénom particuliert
     if (req.body.firstname !== undefined) {
+        // stoque la reque t pour la base de données dans un objet
         user.firstname = {
             "user.firstname": req.body.firstname
         }
+        // ajoute le contenu de l'object au query
         Object.assign(query, user.firstname)
     }
 
+    // test si le client veut les log d'un utilisateur avec un nom particulier
     if (req.body.lastname !== undefined) {
+        // stoque la requete pour la base de donnée dans un objet
         user.lastname = {
             "user.lastname": req.body.lastname
         }
+        // stoque le contenu de l'objet dans le query
         Object.assign(query, user.lastname)
     }
 
+    // test si le client veut les log pour une partie particulières par le biais de son id
     if (req.body.gameId !== undefined) {
+        // stoque la requete pour la base de donnée dans un objet
         game.gameId = {
             "game._id": mongoose.Types.ObjectId(req.body.gameId)
         }
+        // stoque le contenu de l'objet dans le query
         Object.assign(query, game.gameId)
     }
 
+    // test si le client veut les log pour une partie ave un état particulier
     if (req.body.state !== undefined) {
+        // stoque la requete pour la base de donnée dans un objet
         game.state = {
             "game.state": req.body.state
         }
+        // stoque le contenu de l'objet dans le query
         Object.assign(query, game.state)
     }
 
+    // test si le client veut les log pour une partie avec un créateur particulier par le bias de son id
     if (req.body.createurId !== undefined) {
+        // stoque la requete pour la base de donnée dans un objet
         game.createurId = {
             "game.createurId": req.body.createurId
         }
+        // stoque le contenu de l'objet dans le query
         Object.assign(query, game.createurId)
     }
 
+    // test si le client veut les log pour une partie avec un challenger particulier par le bias de son id
     if (req.body.challengerId !== undefined) {
+        // stoque la requete pour la base de donnée dans un objet
         game.challengerId = {
             "game.challengerId": req.body.challengerId
         }
+        // stoque le contenu de l'objet dans le query
         Object.assign(query, game.challengerId)
     }
 
+    // test si le client veut les log d'une année particulière
     if (req.body.year !== undefined) {
+        // stoque la requete pour la base de donnée dans un objet
         var year = {
             "year": req.body.year
         }
+        // stoque le contenu de l'objet dans le query
         Object.assign(query, year)
     }
 
+    // test si le client veut les log d'un mois particulier
     if (req.body.month !== undefined) {
+        // stoque la requete pour la base de donnée dans un objet
         var month = {
             "month": req.body.month
         }
+        // stoque le contenu de l'objet dans le query
         Object.assign(query, month)
     }
 
+    // test si le client veut les log d'un jours particulier
     if (req.body.day !== undefined) {
+        // stoque la requete pour la base de donnée dans un objet
         var day = {
             "day": req.body.day
         }
+        // stoque le contenu de l'objet dans le query
         Object.assign(query, day)
     }
 
+    // test si le client veut les log d'une heure particulière
     if (req.body.hour !== undefined) {
+        // stoque la requete pour la base de donnée dans un objet
         var hour = {
             "hour": req.body.hour
         }
+        // stoque le contenu de l'objet dans le query
         Object.assign(query, hour)
     }
 
+    // test si le client veut les log d'une minutes particulière
     if (req.body.minute !== undefined) {
+        // stoque la requete pour la base de donnée dans un objet
         var minute = {
             "minute": req.body.minute
         }
+        // stoque le contenu de l'objet dans le query
         Object.assign(query, minute)
     }
 
-
+    // récupère les log de la base de données avec le query
     const logs = await Log.find(query)
+    // initialise le message à renvoyer au client
     var logMessage = {}
-    var i = 0
 
+    // intitialise une variable d'incrementation
+    var i = 0
+    // parcour la liste de log
     for (const log of logs) {
+        // intitialise une seconde variable d'incrementation
         var j = 0
+        // initialise une variable ou stoquer les log après filtre
         var newLog = {}
+
+        // test si une partie est associé au log, si non, crée une partie et la remplie d'information vide
         if (log.game === undefined || log.game === null) {
             log.game = {}
             log.game._id = "none"
@@ -304,6 +387,8 @@ exports.listLogs = async (req) => {
             log.game.createurId = "none"
             log.game.challengerId = "none"
         }
+
+        // test si un utilisateur est associé au log, si non, crée un utilisateur et le remplie d'information vide
         if (log.user === undefined || log.user === null) {
             log.user = {}
             log.user._id = "none"
@@ -313,7 +398,10 @@ exports.listLogs = async (req) => {
             log.user.firstname = "none"
             log.user.lastname = "none"
         }
+
+        // test si le client demande les donnée et que le log de cette boucle contient des donnée
         if (req.body.data !== undefined && log.data !== undefined) {
+            // crée un log correspondant à cette boucle avec les information reçu de la base de donnée
             newLog[i] = {
                 logId: log._id,
                 reqParam: log.reqParam,
@@ -334,26 +422,42 @@ exports.listLogs = async (req) => {
                 logHour: log.hour,
                 logMinute: log.minute
             }
+            // initialise les donnée pour ce log
             newLog[i].data = {}
 
+            // parcoure les donnée du log 
             for (const d of log.data) {
-                if (req.body.data.name !== undefined || req.body.data.loc !== undefined || req.body.data.value !== undefined || req.body.data.error !== undefined) {
-                    if (req.body.data.name !== undefined && d.name !== undefined && d.name.toUpperCase().includes(req.body.data.name.toUpperCase())) {
-                        newLog[i].data[j] = d
-                    } else if (req.body.data.loc !== undefined && d.loc !== undefined && d.loc.toUpperCase().includes(req.body.data.loc.toUpperCase())) {
-                        newLog[i].data[j] = d
-                    } else if (req.body.data.value !== undefined && d.value !== undefined && JSON.stringify(d.value).toUpperCase().includes(req.body.data.value.toUpperCase())) {
 
+            
+                // si le client demande une information de tri spécifique pour ces données
+                if (req.body.data.name !== undefined || req.body.data.loc !== undefined || req.body.data.value !== undefined || req.body.data.error !== undefined) {
+                    // si le client demande un une part de nom qui est inclut dans la donnée
+                    if (req.body.data.name !== undefined && d.name !== undefined && d.name.toUpperCase().includes(req.body.data.name.toUpperCase())) {
+                        // stoque la donnée
                         newLog[i].data[j] = d
-                    } else if (req.body.data.error !== undefined && d.error !== undefined && JSON.stringify(d.error).toUpperCase().includes(req.body.data.error.toUpperCase())) {
+                        // si le client demand une part de location qui est inclut dans la donnée
+                    } else if (req.body.data.loc !== undefined && d.loc !== undefined && d.loc.toUpperCase().includes(req.body.data.loc.toUpperCase())) {
+                        // stoque la donnée
+                        newLog[i].data[j] = d
+                        // si le client demande une part de valeur inclu dans la donnée 
+                    } else if (req.body.data.value !== undefined && d.value !== undefined && JSON.stringify(d.value).toUpperCase().replace("{", "").replace("}", "").replace(/"/g, "").includes(JSON.stringify(req.body.data.value).toUpperCase().replace("{", "").replace("}", "").replace(/"/g, ""))) {
+                        // stoque la donnée
+                        newLog[i].data[j] = d
+                        
+                        // si le client demande une part d'erreur inclut dans la donnée
+                    } else if (req.body.data.error !== undefined && d.error !== undefined && JSON.stringify(d.error).toUpperCase().replace("{", "").replace("}", "").replace(/"/g, "").includes(JSON.stringify(req.body.data.error).toUpperCase().replace("{", "").replace("}", "").replace(/"/g, ""))) {
+                        // stoque la donnée
                         newLog[i].data[j] = d
                     }
                 } else {
+                    // stoque la donnée
                     newLog[i].data[j] = d
                 }
+                // incremente la seconde variable d'incrementation
                 j = j + 1
             }
         } else {
+            // construit le log sans les données
             newLog[i] = {
                 logId: log._id,
                 reqParam: log.reqParam,
@@ -375,10 +479,12 @@ exports.listLogs = async (req) => {
                 logMinute: log.minute
             }
         }
+        // ajoute le log dans le message à envoyer au client
         Object.assign(logMessage, newLog)
+        // incremente la première variable d'incrementation
         i = i + 1
     }
-
+    // retourn le message à envoyer au client
     return logMessage
 }
 
