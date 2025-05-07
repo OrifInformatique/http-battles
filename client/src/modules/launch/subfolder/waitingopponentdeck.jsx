30d4e8b1ae88f4b5724b6537219775f0a8a26199
YaelPomy2 <yaelfavresectioninformatique@gmail.com>
2025-05-07 16:52:44 +0200

    Ajout d'élément dans la page d'opponent

--- client/src/modules/launch/subfolder/waitingopponentdeck.jsx
+++ client/src/modules/launch/subfolder/waitingopponentdeck.jsx
@@ -1,58 +1,59 @@
 import React, { useState } from "react";
 
+function WordUnderscored({phrase, i}) {
+    return(<>
+        <div style={{ display:"flex", flexFlow:"column", alignItems:"center", bottom:"20px" }}>
+            <h2>{phrase[0].phrase[i].word.content}</h2>
+            <p style={{position:"relative", fontSize:"100px", height:"20px", bottom:"110px"}}>_</p>
+        </div>
+    </>)
+}
+
 
 const WaitingOpponentDeck = ({phrase}) => {
+    //exemples de messages avec leur useState
     let turnA = "À votre tour";
     let turnB = "Au tour de l'adversaire";
     const [turnStatus, setTurnStatus] = useState(turnB)
 
-
     let actA = "Vous avez loupé votre adversaire"
-    let actB = "Vous avez touché votre adversaire, à votre tour"
+    let actB = "Vous avez touché votre adversaire"
     let actC = "Vous avez gagné"
     const [action, setAction] = useState(actA)
 
     return(<>
-    {/*
-    <div className="waitTitle">
-        <h1>Partie # numéro de la partie  - Initialisation de la phrase</h1>
-    </div>*/}
-    <div className="gridContainer" style={{width:"1568px", position:"relative",height:"1000px", display:"flex", flexFlow:"column nowrap"}}> 
+    <div className="gridContainer" style={{width:"1568px", position:"relative",height:"1000px", display:"flex", flexFlow:"column nowrap", alignItems:"center"}}> 
         
-        <div style={{display:"flex", flexDirection:"row", alignItems:"center", justifyContent:"center",width:"100%", paddingTop:"15px"}}/*className="settingsContainer"*/>
-            <p className="ironPlankText" style={{fontSize:"45px"}}>{turnStatus}</p> 
+        <div className="ironPlankContainer">
+            <img className="ironPlank" src="../assets/images/element/CC0/waitingopponentdeck/element/iron_plank.png"/>
+            <p className="ironPlankText" style={{fontSize:"45px"}}> {turnStatus} </p> 
         </div>
-        <img className="settings" alt="" src="../assets/images/element/CC0/waitingopponentdeck/element/settings-icon.png" />
+        <img className="settings" alt="" src="../assets/images/element/CC0/waitingopponentdeck/element/settings-icon.png"/>
 
         <div className="redblueGridContainer">
             <img className="redGrid" alt="Grille Rouge" src="../assets/images/element/CC0/waitingopponentdeck/fight_grid/opponent_grid_red.png"/>
             <img className="blueGrid" alt="Grille Bleue" src="../assets/images/element/CC0/waitingopponentdeck/fight_grid/player_grid.png"/>
         </div>
 
+        <div className="wordFound">
+           <WordUnderscored phrase={phrase} i={0}/>
+           <WordUnderscored phrase={phrase} i={1}/>
+           <WordUnderscored phrase={phrase} i={2}/>
+           <WordUnderscored phrase={phrase} i={3}/>
+           <WordUnderscored phrase={phrase} i={4}/>
+        </div>
+
         <div className="woodContainer">
-        <img className="woodLayout" alt="Layout du bas couleur bois, emplacement du host" src="../assets/images/element/CC0/waitingopponentdeck/layouts/wood_layout.png" />
+            <img className="woodLayout" alt="Layout du bas couleur bois, emplacement du host" src="../../../assets/images/element/CC0/waitingopponentdeck/layouts/wood_layout.png"/>
+            <h1 className="username">Hello</h1>
             <div style={{position:"relative", display:"flex", flexFlow:"column wrap"}}>
                 <p className="woodLayoutText"> {action} </p>
-                <h1>Hello</h1>
+                <img className="imgExplosion"src="../assets/images/element/CC0/waitingopponentdeck/element/explosion.png"/>
             </div>
         </div>
 
     </div>
-        {/* 
-        <h2>Votre phrase est prête</h2>
 
-        <div className="waitPhrase">
-            <p>{phrase[0].phrase[0].word.content} {phrase[0].phrase[1].word.content} {phrase[0].phrase[2].word.content} {phrase[0].phrase[3].word.content} {phrase[0].phrase[4].word.content} </p>
-        </div>
-
-        <button type="submit"> Ce n'est pas votre tour </button>
-        <p style={{fontWeight:"bold"}}>La phrase de votre adversaire</p>
-        <p>__ __ __ __ __</p>
-        <div style={{borderBottom:"2px solid grey",height:"50px",width:"100%"}}></div>
-        <h1>En attente de [username]
-            <p>La partie pourra démarrer dès que [username] a renseigné la phrase</p>
-        </h1>
-        */}
     </>)
 }
 
